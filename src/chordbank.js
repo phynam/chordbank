6b2a2394c050d159189cf55910c1ea7efc559ec5
pieterhynam <pieter@hynam.net>
2017-06-11 22:20:13 -0700

    Built out interval calculator.

--- src/chordbank.js
+++ src/chordbank.js
@@ -1,6 +1,6 @@
 (function() {
 
-    var _chord_library = [
+    var _chord_patterns = [
         {
             type: 'M',
             recipe: ['3', '5']
@@ -66,19 +66,111 @@
         }
     }
 
-    var Chord = function(args) {
+    /**
+     * Return notes at given intervals
+     *
+     * @private
+     */
+    var _IntervalCalculator = function() {
 
-        this.type = args.type;
-        this.recipe = args.recipe;
+        var _note_count = 12;
+
+        var _notes = {
+            'C': 1, 'C#': 2, 'Db': 2, 'D': 3, 'D#': 4, 'Eb': 4, 'E': 5, 'F': 6, 'F#': 7, 'Gb': 7, 'G': 8, 'G#': 9, 'Ab': 9, 'A': 10, 'A#': 11, 'Bb': 11, 'B': 12, 'Cb': 12
+        }
+
+        /**
+         * Return note at a particular interval
+         *
+         * @param root
+         * @param interval
+         * @param type
+         */
+        this.note_at_interval = function(root, interval, type) {
+
+            var key_type;
+            root = root.charAt(0).toUpperCase() + root.slice(1);
+
+            if(_notes[root]) {
+
+                if(root.charAt(1) === 'b') {
+                    key_type = 'flat'
+                } else if(root.charAt(1) === '#') {
+                    key_type = 'sharp'
+                }
+
+                var root_int = _notes[root],
+                    total_int = root_int + interval,
+                    calculated_int = (total_int <= _note_count) ? total_int : total_int - _note_count;
+
+                var result = [];
+                for (var key in _notes) {
+                    if (_notes.hasOwnProperty(key) && _notes[key] == calculated_int) {
+                        result.push(key);
+                    }
+                }
+
+                console.log(result);
+
+                // Find the root in the table, store its interval
+                // Add the interval to the root inerval and return note name(s) at that position
+                // If flat, return the flat, else return the sharp version
+
+            } else {
+                _notify('warn', 'Invalid root note');
+            }
+        }
     }
 
-    var _ChordLibrary = function() {
+    /**
+     * Chord model constructor, responsible for housing chord data
+     *
+     * @param args
+     * @constructor
+     */
+    var Chord = function(args) {
+        this.type = args.type;
+        this.root = args.root;
+    }
 
-        var _chords = [];
+    /**
+     * Chord factory constructor, responsible for creating chords from a given recipe
+     *
+     * @private
+     */
+    var _ChordFactory = function() {
+
+        var _calculator;
 
         function _initialize() {
-            for(var i = 0; i < _chord_library.length; i++) {
-                _chords.push(new Chord(_chord_library[i]));
+            _calculator = new _IntervalCalculator();
+        }
+
+        /**
+         * Create a chord from a given root and recipe
+         *
+         * @param args
+         */
+        this.create_chord = function(args) {
+            _calculator.note_at_interval(args.root, 1);
+            //return new Chord(args);
+        }
+
+        _initialize();
+    }
+
+    /**
+     * Pattern library model constructor
+     *
+     * @private
+     */
+    var _PatternLibrary = function() {
+
+        var _patterns = [];
+
+        function _initialize() {
+            for(var i = 0; i < _chord_patterns.length; i++) {
+                _patterns.push(_chord_patterns[i]);
             }
         }
 
@@ -87,27 +179,29 @@
          * @param callback
          */
         this.find = function(callback) {
-            for(var i = 0; i < _chords.length; i++) {
-                if(callback(_chords[i])) {
-                    return _chords[i];
+            for(var i = 0; i < _patterns.length; i++) {
+                if(callback(_patterns[i])) {
+                    return _patterns[i];
                 }
             }
-            _notify('warn', 'No matching chords found');
+            _notify('warn', 'No matching chord patterns found');
         }
 
         _initialize();
     }
 
-    var ChordBank = function(arguments) {
+    var ChordBank = function() {
 
         var _library;
+        var _factory;
 
         function _initialize() {
-            _library = new _ChordLibrary();
+            _library = new _PatternLibrary();
+            _factory = new _ChordFactory();
         }
 
         /**
-         * Validate a set of arguments against certain rules, returns boolean.
+         * Validate a set of parameters against certain rules, returns boolean.
          * Executes callback if valid
          *
          * @param validations
@@ -116,7 +210,7 @@
          * @returns {boolean}
          * @private
          */
-        function _validate_arguments(validations, parameters, callback) {
+        function _validate_parameters(validations, parameters, callback) {
 
             var required_fields = validations.requires,
                 valid = true;
@@ -144,28 +238,26 @@
         /**
          * Create a new chord object with given parameters
          *
-         * @param arguments
+         * @param parameters
          */
-        this.create = function(arguments) {
+        this.create = function(parameters) {
 
-            _validate_arguments({
+            _validate_parameters({
                 requires: ['root', 'type']
-            }, arguments, function(args) {
+            }, parameters, function(args) {
 
-                var chord = _library.find(function(chord) {
+                var pattern = _library.find(function(chord) {
                     return chord.type === args.type;
                 });
 
-                console.log(chord);
+                var params = {
+                    root: args.root,
+                    type: pattern.type,
+                    recipe: pattern.recipe
+                };
+                console.log(_factory.create_chord(params));
             });
         };
-
-        /**
-         * Return the chord library
-         */
-        this.library = function() {
-
-        }
 
         _initialize();
     }
